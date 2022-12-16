7edab19651077317b732517136d60510b078c247
Virej Dasani <virej@Virejs-MBP.broadband>
2022-12-16 15:13:00 +0000

    sUbScRiBe To ViReJ

--- content.js
+++ content.js
@@ -1,16 +1,18 @@
 const gifs = [
-  "https://media.tenor.com/HuyTG8nfZ90AAAAC/rick_astley_oh_reallyy.gif",
-  "https://media.tenor.com/o656qFKDzeUAAAAC/rick-astley-never-gonna-give-you-up.gif",
-  "https://media.tenor.com/IAWKXaW_52sAAAAd/rickroll.gif",
-  "https://media.tenor.com/fGfybuB14fwAAAAM/rick-rolled.gif",
-  "https://media.tenor.com/KYhQu2k8MioAAAAM/rick-rolled.gif",
-  "https://media.tenor.com/-zKOgYpN_jsAAAAM/velocity-edit-rick-astley.gif",
-  "https://media.tenor.com/hzxb6Dl7yFoAAAAC/izbandut-i%CC%87zbandut.gif",
-  "https://media.tenor.com/j7syomE0JA4AAAAC/rick-roll-rick-astley.gif",
-  "https://media.tenor.com/rkg-xjgb8tMAAAAd/rickroll.gif",
+  "./assets/rick001.gif",
+  "./assets/rick002.gif",
+  "./assets/rick003.gif",
+  "./assets/rick004.gif",
+  "./assets/rick005.gif",
+  "./assets/rick006.gif",
+  "./assets/rick007.gif",
+  "./assets/rick008.gif",
+  "./assets/rick009.gif",
 ];
 
 const images = document.getElementsByTagName("img");
 for (let i = 0; i < images.length; i++) {
-  images[i].src = gifs[Math.floor(Math.random() * gifs.length)];
+  images[i].src = chrome.runtime.getURL(
+    gifs[Math.floor(Math.random() * gifs.length)]
+  );
 }
